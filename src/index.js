75698992d2b8da7fd60c7d5080e2a571aca4b323
tashasun <natalia.limbakh@gmail.com>
2021-08-14 21:21:01 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,77 @@
 module.exports = function toReadable (number) {
+// const toReadable = (number) => {
+    let result = "";
+    let arrTens = [
+        "zerro",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let untilTwenty = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    if (number < 0) {
+        return (result = null);
+    }
+    if (number < 20) {
+        return untilTwenty[number];
+    }
+    let quantityHundred = Math.floor(number / 100);
+    let quantityTens = Math.floor((number % 100) / 10);
+    let quantityUnits = (number % 100) % 10;
+    if (quantityHundred > 0) {
+        result = result + untilTwenty[quantityHundred] + " hundred ";
+        // console.log(quantityHundred);
+    }
+    if (quantityTens >= 2) {
+        result = result + `${arrTens[quantityTens]}`;
+        // console.log(quantityTens);
+        // console.log(quantityUnits);
+    
+        if (quantityUnits >0) {
+        result = result + ` ${untilTwenty[quantityUnits]}`;
+        return result;
+    }
+  };
   
+  if (quantityTens < 2) {
+    if (quantityTens === 0 & quantityUnits === 0)
+    { return result.trim()};
+    let helpUntiltwenty = number % 100
+    result = result + `${untilTwenty[helpUntiltwenty]}`
+    // console.log(helpUntiltwenty)
+  }
+  return result
+
 }
+// console.log(toReadable(120));
+// console.log(toReadable(510));
+// console.log(toReadable(110));
+// console.log(toReadable(999));
+// // console.log(toReadable(10));
+// console.log(toReadable(1));
